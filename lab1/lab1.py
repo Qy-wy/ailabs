368c6127f516e9f9570ca7ada6fd9393e02ecc0c
Qy-wy <lizaliyts@gmail.com>
2026-03-04 19:23:37 +0300

    Add lab1

---
+++ lab1/lab1.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+
+df = pd.read_csv('healthcare-dataset-stroke-data.csv')
+print(df)
+
+num_missing_cols = df.isnull().any().sum()
+print("Количество столбцов с пропусками:", num_missing_cols)
+df_rows = len(df)
+
+print("\nСтолбцы, где пропусков > 5%")
+for col in df:
+    quantity_of_na = df[col].isnull().sum()
+    if (quantity_of_na/df_rows) < 0.05:
+       df.dropna(subset=[col], inplace=True)
+    else:
+        print(col, ": ", df[col].isnull().sum())
+        print("Статистика:\n", df[col].describe())
+
+df['bmi'] = df['bmi'].fillna(df['bmi'].median())
+
+print("\nСтолбцы после заполнения пропусков\n")
+for col in df:
+    print(col)
+    print(df[col].isnull().sum())
+
+num_cols = ['age', 'avg_glucose_level', 'bmi']
+df[num_cols] = scaler.fit_transform(df[num_cols])
+
+df = pd.get_dummies(df, columns=['gender', 'ever_married', 'work_type', 'Residence_type', 'smoking_status'], drop_first=True)
+df.to_csv("processed_ds.csv", index=False)
